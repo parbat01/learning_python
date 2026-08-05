34bb63c7cc8c5d37c4d2de96060b063c1d59dc1a
Parbat Upadhaya <parbatupadhaya2@gmail.com>
2026-08-05 23:49:59 +0545

    Day-01 : Learned Varibles

---
+++ Day-01-Variables/notes.md
@@ -0,0 +1,3 @@
+Today i learned basic of pyhton and variables.
+I built a simple calculator using 2 variables.
+It calcualtes sum, difference, multiplication,division,remainder,and to power.
---
+++ Day-01-Variables/variables.py
@@ -0,0 +1,8 @@
+a=float(input("Enter your 1st number :"))
+b=float(input("Enter your 2nd number :"))
+print("Sum of your numbers is : ", a+b)
+print("Subtraction of your numbers is : ", a-b)
+print("Division of your numbers is : ", a/b)
+print("Multiplication of your numbers is : ", a*b)
+print("Remainder of your numbers is : ", a%b)
+print("Power of your numbers i.e",a,"to the power",b," is : ", a**b)
