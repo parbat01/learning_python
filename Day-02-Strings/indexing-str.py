a4094a4971dd60270d6082ae9b958bd06355f7d1
Parbat Upadhaya <parbatupadhaya2@gmail.com>
2026-08-06 11:35:39 +0545

    Learned string , indexing , slicing , and it's methods

---
+++ Day-02-Strings/indexing-str.py
@@ -0,0 +1,11 @@
+#In this program i'll use indexing concept and print different characters of a word.
+word="Programming"
+a=word[0]
+print(a) #This will print P because the index of a P is 0
+chr=word[1]
+print(chr)  #This will print r because the index of a P is 1
+print(word[2]) #This will print o because the index of a P is 2
+chr=word[3]
+print(chr)  #This will print g because the index of a P is 3
+
+#We can use different methods to print character like above
---
+++ Day-02-Strings/string.py
@@ -0,0 +1,4 @@
+#In this program i'll print my name and it's type 
+name="Parbat Upadhaya"
+print(name)
+print(type(name))
